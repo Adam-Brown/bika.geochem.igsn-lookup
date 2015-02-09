46874e81dd6c006ed0adb6fe7b9b14e02dfa9b1e
Adam Brown <adam.brown@evbane.com>
2015-02-09 14:09:46 +0800

    Adding lookup.php

---
+++ lookup.php
@@ -0,0 +1,66 @@
+<?php
+    $igsn_url = $_GET['igsn_url'];
+    $page = file_get_contents($igsn_url);
+
+    $page = preg_replace('/\n/', '', $page);
+
+    $search_targets = [
+        'Sample Name',
+        "Material",
+        "Classification",
+        "Field Name",
+        "Age (min)",
+        "Age (max)",
+        "Collection Method",
+        "Collection Method Description",
+        "Size",
+        "Geological Age",
+        "Geological Unit",
+        "Comment",
+        "Purpose",
+        "Latitude",
+        "Longitude",
+        "Elevation",
+        "Nav Type",
+        "Physiographic Feature",
+        "Name Of Physiographic Feature",
+        "Location Description",
+        "Locality",
+        "Locality Description",
+        "Country",
+        "State/Province",
+        "County",
+        "City",
+        "Field Program/Cruise",
+        "Platform Type",
+        "Platform Name",
+        "Platform Description",
+        "Launch Type",
+        "Launch Platform Name",
+        "Launch ID",
+        "Collector/Chief Scientist",
+        "Collector/Chief Scientist Detail",
+        "Collection Start Date",
+        "Collection End Date",
+        "Current Archive",
+        "Current Archive Contact Details",
+        "Original Archive",
+        "Original Archive Contact Details",
+        "Depth in Hole (min)",
+        "Depth in Hole (max)",
+        "Parents",
+        "Siblings",
+        "Children"
+    ];
+
+    $data = [];
+
+    foreach ($search_targets as $search_target)
+    {
+        preg_match('#' . preg_quote($search_target, '#') . ':.+?</td>.+?<td.*?>(.+?)<#', $page, $matches);
+        $data[$search_target] = trim($matches[1]);
+    }
+
+	header('Content-Type: application/json');
+    echo json_encode($data);
+?>
